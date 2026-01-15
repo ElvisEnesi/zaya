a10243b28d46bb22d3f17597beb69aee1cfba35b
Elvis Enesi <jattoelvis00@gmail.com>
2026-01-15 03:58:26 +0100

    commit

--- addstudent.php
+++ addstudent.php
@@ -1,14 +1,8 @@
     <?php
         include "database.php";
+        include "doctype.php";
+        include "header.php";
     ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Custech Result Complaint System</title>
-    <link rel="stylesheet" href="style.css">
-</head>
 <body>
     <section class="form">
         <form action="" method="post">
--- header.php
+++ header.php
@@ -5,27 +5,27 @@
         </div>
         <div class="nav">
             <div class="navItems">
-                Y
+                <ion-icon name="home-outline"></ion-icon>
                 <a href="index.php">Home</a>
             </div>
             <div class="navItems">
-                Y
+                <ion-icon name="person-add-outline"></ion-icon>
                 <a href="addstudent.php">Add Students</a>
             </div>
             <div class="navItems">
-                Y
+                <ion-icon name="book-outline"></ion-icon>
                 <a href="view.php">View complains</a>
             </div>
             <div class="navItems">
-                Y
+                <ion-icon name="add-circle-outline"></ion-icon>
                 <a href="make_complain.php">Make a complain</a>
             </div>
             <div class="navItems">
-                Y
+                <ion-icon name="folder-outline"></ion-icon>
                 <a href="history.php">History</a>
             </div>
             <div class="navItems">
-                Y
+                <ion-icon name="log-out-outline"></ion-icon>
                 <a href="logout.php">Log out</a>
             </div>
         </div>
